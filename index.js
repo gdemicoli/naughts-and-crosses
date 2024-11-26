921e12bd990b97e03e8600bb89f1271d33290b98
Gustavo <s3898300@student.rmit.edu.au>
2024-11-26 11:28:22 +1100

    Working program

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const gameBoard = (function () {
     const board = []
 
     for(let i = 0; i < 9; i++){
-        board[i] = null
+        board[i] = ' '
     }
 
     console.log("board is " + board)
@@ -12,20 +12,42 @@ const gameBoard = (function () {
     const getBoard = () => board
 
     const clearBoard = () => {for(let i = 0; i < 9; i++){
-        board[i] = []
+        board[i] = ' '
        };
     }
 
-    return{board, getBoard, clearBoard};
+    const freezeBoard = () => {for(let i = 0; i < 9; i++){
+        board[i] = '*'
+       };
+
+    }
+
+    return{board, getBoard, clearBoard, freezeBoard};
 }) ();
 
 const game = ( function() {
     const players = [
-        {name: 'player1', symbol: 'x'}, 
-        {name: 'player2', symbol: 'o'}     
+        {name: 'Player 1', symbol: 'x', squaresClaimed: []}, 
+        {name: 'Player 2', symbol: 'o', squaresClaimed: []}     
     ]
 
     let playerTurn = players[0]
+
+    const subheader = document.querySelector(".subheader");
+   
+
+    const newTurn = () => {
+        if (playerTurn === players[0]) {
+            playerTurn = players[1]
+            subheader.textContent = `${playerTurn.name}'s turn `
+        }
+        else {
+            playerTurn = players[0]
+            subheader.textContent = `${playerTurn.name}'s turn `
+        }
+    }
+
+    const getPlayerTurn = () => playerTurn
 
     const winningCombos = [
         [0, 1, 2],
@@ -37,10 +59,29 @@ const game = ( function() {
         [0, 4, 8],
         [2, 4, 6],
       ];
+    
+
+    const hasWon = () => {
+        for (let combo of winningCombos) {
+            if (combo.every(val => playerTurn.squaresClaimed.includes(val))) {
+                subheader.textContent = `${playerTurn.name} wins!`
+                return true; // Exits the loop and the function
+            }
+        }
+        return false; // If no combo satisfies the condition
+    };
+
+    const resetPlayers = () => {
+        players.forEach(player => {
+            player.squaresClaimed.length = 0;
+        });
+    };
+    
+
 
     
-      return{players, playerTurn, winningCombos}
-    })
+      return{players, playerTurn, winningCombos, newTurn, getPlayerTurn, hasWon, resetPlayers}
+    }) ();
 
     const clickHandler = (e) => {
         const selectedCell = e.target
@@ -52,55 +93,41 @@ const game = ( function() {
 
     const buttons = document.querySelectorAll(".cell-button")
 
+    console.log("Game board: " + gameBoard.getBoard())
 
+
+   
     buttons.forEach(button => {
         button.addEventListener('click', (e) => {
             const cell = Number(e.target.id)
 
-            if(gameBoard.getBoard[cell] === undefined) {
-                e.target.textContent = game.playerTurn.symbol;
+            if(gameBoard.getBoard()[cell] === ' ') {
+
+                e.target.textContent = game.getPlayerTurn().symbol;
+                gameBoard.getBoard()[cell] = game.getPlayerTurn().symbol;
+                game.getPlayerTurn().squaresClaimed.push(cell);
+
+                if(game.hasWon()){
+                    gameBoard.freezeBoard()
+                    console.log("finished")
+                }
+
+                else {
+                    game.newTurn()
+                }
             }
+
         })
 
     })
 
-    // buttons.addEventListener("click", (e) =>{
-    //    console.log(e.target);
-    // })
-
-    // const button = document.querySelector(".newgame-button");
-    // console.log(button)
-
-
-    // button.addEventListener("mouseover", (something) => {
-    //     console.log(something)
-    //     console.log("e.target below")
-    //     console.log(something.target)
-    // })
-
-    // button.textContent = "we have picked up button"
+    const newGameButton = document.querySelector(".newgame-button")
+    newGameButton.addEventListener('click', () => {
+        gameBoard.clearBoard();
+        buttons.forEach(button => button.textContent = '');
+        game.resetPlayers();
 
 
+     })
 
-
-
-
-
-
-
-
-
-// const calculator = (function () {
-//     const add = (a, b) => a + b;
-//     const sub = (a, b) => a - b;
-//     const mul = (a, b) => a * b;
-//     const div = (a, b) => a / b;
-//     return { add, sub, mul, div };
-//   })();
-  
-//   calculator.add(3,5); // 8
-//   calculator.sub(6,2); // 4
-//   calculator.mul(14,5534); // 77476
-  
-// )
-
+    
